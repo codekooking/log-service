8e4d604d04d47a0b76e7119b041c27812724e2eb
Thuan Nguyen [PC] <thuannguyenit@gmail.com>
2018-05-12 15:33:44 +0700

    Add reference url

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ This is repository used to store application log message by using Spring-boot, R
 ## Swagger
 - Swagger UI http://localhost:8080/swagger-ui.html
 - Swagger API docs http://localhost:8080/v2/api-docs
+
+## Reference
+- http://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
--- src/main/java/com/codekooking/logservice/configs/SwaggerConfig.java
+++ src/main/java/com/codekooking/logservice/configs/SwaggerConfig.java
@@ -8,7 +8,7 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import static springfox.documentation.builders.PathSelectors.regex;
+
 import java.util.Collections;
 
 @Configuration
@@ -20,8 +20,10 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.codekooking.logservice.controllers"))
-                .paths(regex("/example.*"))
+                /*.paths(PathSelectors.regex("/example.*"))*/
                 .build()
+                /*.securitySchemes(Arrays.asList(securityScheme()))
+                .securityContexts(Arrays.asList(securityContext()))*/
                 .apiInfo(apiInfo());
     }
 	
@@ -29,9 +31,50 @@ public class SwaggerConfig {
 	     return new ApiInfo(
 	       "Log Service REST API", 
 	       "This is REST API used to store Application log messages to RabbitMQ", 
-	       "API TOS", 
+	       "V1.0", 
 	       "Terms of service", 
 	       new Contact("Code Kooking", "http://www.codekooking.com", "thuannguyenit@gmail.com"), 
 	       "Apache License 2.0", "http://www.apache.org/licenses", Collections.emptyList());
 	}
+	
+	/*
+	@Bean
+	public SecurityConfiguration security() {
+	    return SecurityConfigurationBuilder.builder()
+	        .clientId(CLIENT_ID)
+	        .clientSecret(CLIENT_SECRET)
+	        .scopeSeparator(" ")
+	        .useBasicAuthenticationWithAccessCodeGrant(true)
+	        .build();
+	}
+	
+	private SecurityContext securityContext() {
+	    return SecurityContext.builder()
+	      .securityReferences(Arrays.asList(new SecurityReference("spring_oauth", scopes())))
+	      .forPaths(PathSelectors.regex("/secured.*"))
+	      .build();
+	}
+	
+	private SecurityScheme securityScheme() {
+	    GrantType grantType = new AuthorizationCodeGrantBuilder()
+	        .tokenEndpoint(new TokenEndpoint(AUTH_SERVER + "/token", "oauthtoken"))
+	        .tokenRequestEndpoint(
+	          new TokenRequestEndpoint(AUTH_SERVER + "/authorize", CLIENT_ID, CLIENT_ID))
+	        .build();
+	 
+	    SecurityScheme oauth = new OAuthBuilder().name("spring_oauth")
+	        .grantTypes(Arrays.asList(grantType))
+	        .scopes(Arrays.asList(scopes()))
+	        .build();
+	    return oauth;
+	}
+	
+	private AuthorizationScope[] scopes() {
+	    AuthorizationScope[] scopes = { 
+	      new AuthorizationScope("read", "for read operations"), 
+	      new AuthorizationScope("write", "for write operations"), 
+	      new AuthorizationScope("foo", "Access foo API") };
+	    return scopes;
+	}
+	*/
 }
